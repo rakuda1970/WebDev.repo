58ccd125508e0ac81c94a93c36963a6b6df57929
BRIANA-PC\BrianA <briananderson@arielpremium.com>
2023-04-26 18:20:46 -0500

    initial commit

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('content-title')</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    @include('layout.header')
+    <div class="container my-5">
+    @yield('content')
+    </div>
+    @include('layout.footer')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="main.js"></script>
+  </body>
+</html>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,132 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">WebDev.repo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Nav
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/">Home</a></li>
+                <a class="dropdown-item" href="sitemap">Sitemap</a>
+              </ul>
+            </li>
+
+            
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Reference
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/html">html</a></li>
+                <li><a class="dropdown-item" href="/css">css</a></li>
+                <li><a class="dropdown-item" href="/php">PHP</a></li>
+                <li><a class="dropdown-item" href="/ecma">javascript</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="/bootstrap">Bootstrap</a></li>
+                <li><a class="dropdown-item" href="/bootstrap">JQuery</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="/laravel">Laravel</a></li>
+                <li><a class="dropdown-item" href="/mysql">MySQL</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="/video-media">Videos/Media</a></li>
+                <li><a class="dropdown-item" href="/mysql">Images</a></li>
+                <li><a class="dropdown-item" href="/pdf">PDF</a></li>
+                <li><a class="dropdown-item" href="/aws-s3">AWS S3</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="/booklist">Books</a></li>
+              </ul>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dev
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/bug-tracking">Bug Tracking</a></li>
+              <li><a class="dropdown-item" href="/version-control">Version Control</a></li>
+              <li><a class="dropdown-item" href="/version-control">Apache</a></li>
+              <li><a class="dropdown-item" href="/linux">Linux</a></li>
+              <li><a class="dropdown-item" href="/dev-ide">IDE</a></li>
+              <li><a class="dropdown-item" href="/usability">Usability</a></li>
+              </ul>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Recipes
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/blog-news">Blog/News</a></li>
+                <li><a class="dropdown-item" href="/event-calendar">Event Calendar</a></li>
+                <li><a class="dropdown-item" href="/todo">Todo List</a></li>
+                <li><a class="dropdown-item" href="/carousels">Carousels</a></li>
+                <li><a class="dropdown-item" href="/navigation">Navigation</a></li>
+                <li><a class="dropdown-item" href="/dropdowns">Dropdowns</a></li>
+                <li><a class="dropdown-item" href="/accordions">Accordions</a></li>
+                <li><a class="dropdown-item" href="/popups">Popups</a></li>
+                <li><a class="dropdown-item" href="/js-modals">Modals</a></li>
+                <li><a class="dropdown-item" href="/tag-cloud">Tag Cloud</a></li>
+              </ul>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                E-commerce
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/product-index">Product Index</a></li>
+                <li><a class="dropdown-item" href="/product-categories">Product Categories</a></li>
+                <li><a class="dropdown-item" href="/product-sales">Sales</a></li>
+                <li><a class="dropdown-item" href="/index-sorting">Index Sorting</a></li>
+                <li><a class="dropdown-item" href="/product-detail">Product Detail</a></li>
+                <li><a class="dropdown-item" href="/product-index">Membership</a></li>
+                <li><a class="dropdown-item" href="/product-categories">Authentication</a></li>
+                <li><a class="dropdown-item" href="/product-detail">Shopping Carts</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Website Stats/Data
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/google-analytics">Google Analytics</a></li>
+              <li><a class="dropdown-item" href="/awstats">AWStats</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Website Security
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/security-proactive">Being Proactive</a></li>
+              <li><a class="dropdown-item" href="/security-logging">Logging</a></li>
+              <li><a class="dropdown-item" href="/security-ban-block">Banning &amp; Blocking</a></li>
+              <li><a class="dropdown-item" href="/security-alerts">Alerts</a></li>
+              <li><a class="dropdown-item" href="/security-track-trace">Tracking and Tracing</a></li>
+              <li><a class="dropdown-item" href="/security-social-engineer">Social Engineering</a></li>
+              <li><a class="dropdown-item" href="/security-auth-access">Authentication and access control </a></li>
+              <li><a class="dropdown-item" href="/security-webapp">Web application security </a></li>
+              <li><a class="dropdown-item" href="/security-webapp">Endpoint security</a></li>
+              <li><a class="dropdown-item" href="/security-data-protect">Data protection</a></li>
+              <li><a class="dropdown-item" href="/security-data-validate">Data validate</a></li>
+              <li><a class="dropdown-item" href="/security-data-validate">Incident response</a></li>
+              <li><a class="dropdown-item" href="/security-compliance">Compliance and regulatory requirements</a></li>
+              <li><a class="dropdown-item" href="/security-compliance">Cloud security</a></li>
+              <li><a class="dropdown-item" href="/security-user-awareness"> User awareness and training</a></li>
+              <li><a class="dropdown-item" href="/security-user-awareness"> Contingency Plan</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
