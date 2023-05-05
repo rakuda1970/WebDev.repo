e12ee242126316730b28e90c798279a139eb9d6a
BRIANA-PC\BrianA <briananderson@arielpremium.com>
2023-05-05 08:58:29 -0500

    Upgrade to laravel 10

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,17 @@
-@extends('layout.app')
-@section('content-title') Sales Dashboard @endsection
-@section('content')
-<h1>Sales Dashboard</h1>
-<div class="col-lg-8 px-0">
-    <p class="fs-5">You've successfully loaded up the Bootstrap starter example. It includes <a href="https://getbootstrap.com/">Bootstrap 5</a> via the <a href="https://www.jsdelivr.com/package/npm/bootstrap">jsDelivr CDN</a> and includes an additional CSS and JS file for your own code.</p>
-    <p>Feel free to download or copy-and-paste any parts of this example.</p>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-    <hr class="col-1 my-4">
-
-    <a href="https://getbootstrap.com" class="btn btn-primary">Read the Bootstrap docs</a>
-    <a href="https://github.com/twbs/examples" class="btn btn-secondary">View on GitHub</a>
-</div>
-@endsection
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
